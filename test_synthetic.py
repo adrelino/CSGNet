161f21ab8c329250e370083aa45f3290a83e57a6
Adrian Haarbach <mail@adrian-haarbach.de>
2020-04-15 16:57:02 +0200

    refactored network part of test_synthetic into Evaluator class

--- test_synthetic.py
+++ test_synthetic.py
@@ -17,8 +17,6 @@ from src.utils.train_utils import prepare_input_op, chamfer
 
 config = read_config.Config("config_synthetic.yml")
 model_name = config.pretrain_modelpath.split("/")[-1][0:-4]
-encoder_net = Encoder()
-encoder_net.cuda()
 
 data_labels_paths = {3: "data/synthetic/one_op/expressions.txt",
                      5: "data/synthetic/two_ops/expressions.txt",
@@ -46,26 +44,43 @@ assert len(generator.unique_draw) == 400
 data_labels_paths = {3:data_labels_paths[3]}
 dataset_sizes = {3:dataset_sizes[3]}
 
-imitate_net = ImitateJoint(hd_sz=config.hidden_size,
-                           input_size=config.input_size,
-                           encoder=encoder_net,
-                           mode=config.mode,
-                           num_draws=len(generator.unique_draw),
-                           canvas_shape=config.canvas_shape)
 
-imitate_net.cuda()
-if config.preload_model:
-    print("pre loading model")
-    pretrained_dict = torch.load(config.pretrain_modelpath)
-    imitate_net_dict = imitate_net.state_dict()
-    pretrained_dict = {
-        k: v
-        for k, v in pretrained_dict.items() if k in imitate_net_dict
-    }
-    imitate_net_dict.update(pretrained_dict)
-    imitate_net.load_state_dict(imitate_net_dict)
+class Evaluator():
+    def __init__(self):
+        encoder_net = Encoder()
+        encoder_net.cuda()
+        self.imitate_net = ImitateJoint(hd_sz=config.hidden_size,
+                                   input_size=config.input_size,
+                                   encoder=encoder_net,
+                                   mode=config.mode,
+                                   num_draws=400, #len(generator.unique_draw),
+                                   canvas_shape=config.canvas_shape)
+        self.imitate_net.cuda()
+        if config.preload_model:
+            print("pre loading model")
+            pretrained_dict = torch.load(config.pretrain_modelpath)
+            imitate_net_dict = self.imitate_net.state_dict()
+            pretrained_dict = {
+                k: v
+                for k, v in pretrained_dict.items() if k in imitate_net_dict
+            }
+            imitate_net_dict.update(pretrained_dict)
+            self.imitate_net.load_state_dict(imitate_net_dict)
 
-imitate_net.eval()
+        self.imitate_net.eval()
+
+    def test(self, data_):
+        labels = np.zeros((config.batch_size, max_len), dtype=np.int32)
+        one_hot_labels = prepare_input_op(labels,len(generator.unique_draw))
+        one_hot_labels = Variable(torch.from_numpy(one_hot_labels)).cuda()
+        data = Variable(torch.from_numpy(data_)).cuda()
+        test_output = self.imitate_net.test([data, one_hot_labels, max_len])
+        pred_images, correct_prog, pred_prog = parser.get_final_canvas(
+            test_output,
+            if_just_expressions=False,
+            if_pred_images=True)
+        return pred_images, pred_prog
+
 max_len = max(data_labels_paths.keys())
 parser = ParseModelOutput(generator.unique_draw, max_len // 2 + 1,
                           max_len, config.canvas_shape)
@@ -77,14 +92,14 @@ for k in dataset_sizes.keys():
     test_batch_size = config.batch_size
     total_size += (dataset_sizes[k][1] // test_batch_size) * test_batch_size
 
-
-imitate_net.eval()
 over_all_CD = {}
 Pred_Prog = []
 Targ_Prog = []
 metrics = {}
 programs_tar = {}
 programs_pred = {}
+
+evaluator = Evaluator()
 
 #for jit in [False]:
 jit = False
@@ -109,17 +124,9 @@ with torch.no_grad():
         for i in range(dataset_sizes[k][1] // test_batch_size):
             print(k, i, dataset_sizes[k][1] // test_batch_size)
             data_, labels = next(test_gen_objs[k])
-            one_hot_labels = prepare_input_op(labels,
-                                              len(generator.unique_draw))
-            one_hot_labels = Variable(torch.from_numpy(one_hot_labels)).cuda()
-            data = Variable(torch.from_numpy(data_)).cuda()
-            labels = Variable(torch.from_numpy(labels)).cuda()
-            test_output = imitate_net.test([data, one_hot_labels, max_len])
-            pred_images, correct_prog, pred_prog = parser.get_final_canvas(
-                test_output,
-                if_just_expressions=False,
-                if_pred_images=True)
+            pred_images, pred_prog = evaluator.test(data_)
             target_images = data_[-1, :, 0, :, :].astype(dtype=bool)
+            labels = Variable(torch.from_numpy(labels)).cuda()
             targ_prog = parser.labels2exps(labels, k)
 
             programs_tar[jit] += targ_prog
